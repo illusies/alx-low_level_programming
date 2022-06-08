b085baf37698bdf34f3d3e0d0cf18b6ce3e5939c
illusies <john_illusies@yahoo.com>
2022-06-08 00:48:37 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,29 +8,21 @@
  */
 void print_to_98(int n)
 {
-	int i = 0;
-
-	if (n > 98)
+	if (n < 98)
 	{
-		for (i = n; i >= 98; i--)
+		while (n < 98)
 		{
-			if (i != n)
-			{
-				printf(", ");
-			}
-			printf("%d", i);
+			printf("%d, ", n);
+			n++;
 		}
 	}
-	else
+	else if (n > 98)
 	{
-		for (i = n; i <= 98 ; i++)
+		while (n > 98)
 		{
-			if (i != n)
-			{
-				printf(", ");
-			}
-			printf("%d", i);
+			printf("%d, ", n);
+			n--;
 		}
 	}
-	putchar('\n');
+	printf("98\n");
 }
