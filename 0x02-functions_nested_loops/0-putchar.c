4e8fe0ae929b9d5bc8210b043dabccbfc16a1eb7
John McDove <96707236+illusies@users.noreply.github.com>
2022-06-08 02:17:13 -0500

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,41 +1,24 @@
 #include "main.h"
 
-
-
 /**
+ * main - program that prints _putchar
  *
- *  * main -  program that prints _putchar
+ * Return: (0)
  *
- *   *
- *
- *    * Return: (0)
- *
- *     */
+ */
 
 int main(void)
-
 {
-
-		int i = 0;
-
-			char c[10] = "_putchar";
-
-				
-
-				while (i < 8)
-
-						{
-
-									_putchar(c[i]);
-
-											i++;
-
-												}
-
-					
-
-					_putchar('\n');
-
-						return (0);
-
+	int i = 0;
+	char c[10] = "_putchar";
+	
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	
+	_putchar('\n');
+	
+	return (0);
 }
