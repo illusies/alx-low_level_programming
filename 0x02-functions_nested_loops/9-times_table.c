9bf9871600e955e1d6e63d5322e79e61eb6e59c8
illusies <john_illusies@yahoo.com>
2022-06-08 00:39:32 -0700

    7. There are only 3 colors, 10 digits, and 7 notes its what we do with them thats important

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,20 +6,38 @@
  *
  * Return: void
  */
+
 void times_table(void)
 {
-	int i, j;
-	int total;
-
-	for (i = 0; i <= 9; i++)
+	int row, column, product, tens, ones;
+	
+	for (row = 0; row <= 9; row++)
 	{
-		for (j = 0; j <= 8; j++)
+		for (column = 0; column <= 9; column++)
 		{
-			total = i * j;
-			printf("%3d,", total);
+			product = row * column;
+			tens = product / 10;
+			ones = product % 10;
+			
+			if (column == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
 		}
-		printf("3%d", i * 9);
-
-		printf("\n");
+		_putchar('\n');
 	}
 }
