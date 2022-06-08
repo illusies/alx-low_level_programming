f55dbe5bbd3b1fda5e9fc57744810dee3ba45303
illusies <john_illusies@yahoo.com>
2022-06-08 00:13:57 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,41 @@
 #include "main.h"
 
+
+
 /**
- * main - Program that prints _putchar
  *
- * Return: (0)
- */
+ *  * main -  program that prints _putchar
+ *
+ *   *
+ *
+ *    * Return: (0)
+ *
+ *     */
+
 int main(void)
+
 {
-	int i = 0;
-	char c[10] = "_putchar";
-	
-	while (i < 9)
-	{
-		_putchar(c[i]);
-		i++;
-	}
-	
-	_putchar('\n');
-	return (0);
+
+		int i = 0;
+
+			char c[10] = "_putchar";
+
+				
+
+				while (i < 8)
+
+						{
+
+									_putchar(c[i]);
+
+											i++;
+
+												}
+
+					
+
+					_putchar('\n');
+
+						return (0);
+
 }
