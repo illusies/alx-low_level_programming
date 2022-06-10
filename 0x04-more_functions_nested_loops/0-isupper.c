02c54e38a102f333521ac4e7d4c576329a37ca3e
illusies <john_illusies@yahoo.com>
2022-06-09 19:09:01 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * Return: (0) Success
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+	
+	return (0);
+}
