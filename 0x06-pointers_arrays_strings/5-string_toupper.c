19305d8cf62fb8251dd4d81933c17639a32895c2
illusies <john_illusies@yahoo.com>
2022-06-15 18:53:05 -0700

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *string_toupper - function that changes all
+ * lowercase letters of a string to uppercase
+ * @str: pointer
+ * Return: char
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] <= 'z' && str[i] >= 'a')
+			str[i] -= 32;
+	}
+
+	return (str);
+}
