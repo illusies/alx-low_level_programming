6868ba6deb6fee141e1e7a759ea5c4db78e04360
illusies <john_illusies@yahoo.com>
2022-06-15 18:12:11 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,20 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int a = -1, i;
-	for (i = 0; dest[i] != '0\'; i++);
+	int i = 0, j = 0;
 
-	do {
-		a++;
-		dest[i] = src[a];
+	while (dest[i] != '\0')
+	{
 		i++;
-	} while (src[a] != '0\');
+	}
 
+	while (src[j] != '0\')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '0\';
 	return (dest);
 }
