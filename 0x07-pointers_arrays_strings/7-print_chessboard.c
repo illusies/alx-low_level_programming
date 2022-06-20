c1d1c0fbfd4975c9d3b918be90ad79c33ea0de00
illusies <john_illusies@yahoo.com>
2022-06-20 14:09:41 -0700

    6. Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,32 +1,20 @@
 #include "main.h"
 
 /**
- * _strstr - function the prints a chessboard
- * @haystack: pointer to char
- * @needle: pointer to char
- * Return: 0
+ * print_chessboard - prints a chessboard
+ * @a: matrix
  */
 
-char *_strstr(char *haystack, char *needle)
+void print_chessboard(char (*a)[8])
 {
-	char *result = haystack, *fneedle = needle;
+	int i, j;
 
-	while (*haystack)
+	for (i = 0; i < 8; i++)
 	{
-		while (*needle)
+		for (j = 0; j < 8; j++)
 		{
-			if (*haystack++ != *needle++)
-			{
-				break;
-			}
+			_putchar(*(*(i + a) + j));
 		}
-		if (!*needle)
-		{
-			return (result);
-		}
-		needle = fneedle;
-		result++;
-		haystack = result;
+		_putchar('\n');
 	}
-	return (0);
 }
