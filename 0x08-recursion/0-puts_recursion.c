126f264cab1376d984991f6498edd495bf96ef4a
illusies <john_illusies@yahoo.com>
2022-06-22 10:06:38 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,15 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	char *s1 = s;
+	
+	if (*s1 != '\0')
 	{
-		_putchar('\n');
-		return;
-	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+		_putchar(*s);
+		s1++;
+		_puts_recursion(s1);
+													}
+	else
+	_putchar('\n');
+
 }
