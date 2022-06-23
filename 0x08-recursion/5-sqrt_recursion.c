5ff8d227bd082533bcf467200f8f598bacd76e8c
illusies <john_illusies@yahoo.com>
2022-06-22 19:16:06 -0700

    5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,7 +9,7 @@
 
 int _sqrt_recursion(int n)
 {
-	return (squared(n, 1));
+	return (square(n, 1));
 }
 
 /**
@@ -19,12 +19,12 @@ int _sqrt_recursion(int n)
  * Return: int
  */
 
-int squared(int n, int val)
+int square(int n, int val)
 {
 	if (val * val == n)
 		return (val);
 	else if (val * val < n)
-		return (squared(n, val + 1));
+		return (square(n, val + 1));
 	else
 		return (-1);
 }
