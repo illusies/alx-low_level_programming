c0bff7b577f1817680a08f9f2396941aae7cf41a
illusies <john_illusies@yahoo.com>
2022-06-24 17:31:48 -0700

    0. It aint what they call you, its what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: int
+ * @argv: char
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argv - 1]);
+	return (0);
+}
+
