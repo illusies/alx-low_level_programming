708eb0ffd09ec1aaa0f3a8519344946e4d606e83
John McDove <96707236+illusies@users.noreply.github.com>
2022-06-25 21:05:22 -0500

    Files for Library creation

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* _strspn - function that gets the length of a 
+* prefix substring.
+* @s: String where substring will look.
+* @accept: Substring of accepted chars.
+* Return: Length of occurrence.
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int c = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				c++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+				return (c);
+		}
+		s++;
+	}
+	return (c);
+}
